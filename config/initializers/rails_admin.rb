578595198e2ec7b534100e32b089d93f9be30c11
RomanTsibii <romanot70@gmail.com>
2021-05-12 00:40:05 +0300

    updates Devise layouts

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -3,10 +3,10 @@ RailsAdmin.config do |config|
   ### Popular gems integration
 
   ## == Devise ==
-  # config.authenticate_with do
-  #   warden.authenticate! scope: :user
-  # end
-  # config.current_user_method(&:current_user)
+  config.authenticate_with do
+    warden.authenticate! scope: :user
+  end
+  config.current_user_method(&:current_user)
 
   ## == CancanCan ==
   # config.authorize_with :cancancan
@@ -22,6 +22,9 @@ RailsAdmin.config do |config|
   ## == Gravatar integration ==
   ## To disable Gravatar integration in Navigation Bar set to false
   # config.show_gravatar = true
+
+  # Visible models in admins
+  config.included_models = %w[User]
 
   config.actions do
     dashboard                     # mandatory
